6985ec89464027569ba312a4909093bd609022e7
1Alexander1-oss <kolesnikov.1994@bk.ru>
2021-09-11 16:02:37 +0300

    Add class Dogs + Array

---
+++ src/main/kotlin/Dogs.kt
@@ -0,0 +1,52 @@
+fun main() {
+//    val bonya = Dog("bonya")
+//    val juja = Dog(name = "juja")
+//    val lisa = Dog(name = "lisa")
+//
+////    val animals = arrayOf( "bonya", "juja", "lisa")
+//    val animals = arrayOf( bonya, juja, lisa)
+//    for (dog in animals) {
+//        dog.voice()
+//    }
+//
+//}
+//
+//class Dog(val name: String) {
+//    fun voice() {
+//        println("$name Gav")
+//    }
+
+
+//    val bonya = Bonya("bonya")
+//    val lisa = Lisa("lisa")
+//    val juja = Juja("juja")
+//
+//    bonya.voice()
+//    lisa.voice()
+//    juja.voice()
+
+//    val dogs = arrayOf(Bonya, Lisa, Juja)
+//    for (dog in dogs){
+//        dog.voice()
+//    }
+}
+
+
+//open class Dog(open val name: String) {
+//    open fun voice() {
+//        println("default $name bark")
+//    }
+//}
+//
+//class Bonya(override val name: String) : Dog(name) {
+//    override fun voice(){
+//        println("bork")
+//    }
+//}
+//
+//class Lisa(override val name: String) : Dog(name) {
+//}
+//
+//class Juja(override val name: String) : Dog(name) {
+//}
+
